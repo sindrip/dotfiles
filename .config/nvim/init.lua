7c41d93864b432cc6a0d5f0ad035815ab36a3317
Sindri P. Ingimundarson <sindripetur@gmail.com>
2022-08-14 21:21:54 +0200

    gitignore

---
+++ .config/nvim/init.lua
@@ -0,0 +1,219 @@
+require "sindrip.packer"(function(use)
+  use "wbthomason/packer.nvim"
+  use "neovim/nvim-lspconfig"
+  use "williamboman/mason.nvim"
+  use "williamboman/mason-lspconfig.nvim"
+  use "mhartington/formatter.nvim"
+  use "shaunsingh/nord.nvim"
+  use "lewis6991/gitsigns.nvim"
+  use {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.0",
+    requires = { { "nvim-lua/plenary.nvim" } },
+  }
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "L3MON4D3/LuaSnip"
+  use "saadparwaiz1/cmp_luasnip"
+end)
+
+local cmp = require "cmp"
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  window = {
+    --completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert {
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  },
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    --{ name = 'vsnip' }, -- For vsnip users.
+    { name = "luasnip" }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = "buffer" },
+  }),
+}
+
+-- Basic Settings
+vim.g.mapleader = " "
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = "→ ",
+  nbsp = "␣",
+  trail = "·",
+  -- Cursor disappears with nord
+  --space = "·",
+  extends = "⟩",
+  precedes = "⟨",
+}
+vim.opt.scrolloff = 3
+vim.opt.sidescrolloff = 5
+vim.opt.cursorline = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = -1
+vim.opt.shiftround = true
+vim.opt.smartindent = true
+--vim.opt.clipboard = "unnamedplus"
+vim.opt.hidden = true
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.signcolumn = "number"
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.cmd [[colorscheme nord]]
+
+require("gitsigns").setup()
+require("mason").setup()
+-- prettier, stylua manually
+require("mason-lspconfig").setup {
+  automatic_installation = true,
+}
+
+vim.keymap.set(
+  "n",
+  "<leader>e",
+  vim.diagnostic.open_float,
+  { noremap = true, silent = true }
+)
+vim.keymap.set(
+  "n",
+  "[d",
+  vim.diagnostic.goto_prev,
+  { noremap = true, silent = true }
+)
+vim.keymap.set(
+  "n",
+  "]d",
+  vim.diagnostic.goto_next,
+  { noremap = true, silent = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader>q",
+  vim.diagnostic.setloclist,
+  { noremap = true, silent = true }
+)
+
+local on_attach = function(_, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  --vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  --vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+  --vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+  --vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+  --vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+  --vim.keymap.set("n", "<space>wl", function()
+  --  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  --end, bufopts)
+  --vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+  --vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+  --vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
+end
+
+local capabilities = require("cmp_nvim_lsp").update_capabilities(
+  vim.lsp.protocol.make_client_capabilities()
+)
+
+require("lspconfig").rust_analyzer.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+require("lspconfig").elixirls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+require("lspconfig").yamlls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+require("lspconfig").sumneko_lua.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
+}
+
+require("formatter").setup {
+  filetype = {
+    lua = {
+      require("formatter.filetypes.lua").stylua,
+    },
+    rust = {
+      require("formatter.filetypes.rust").rustfmt,
+    },
+    elixir = {
+      require("formatter.filetypes.elixir").mixformat,
+    },
+    yaml = {
+      require("formatter.filetypes.yaml").prettier,
+    },
+    ["*"] = {
+      require("formatter.filetypes.any").remove_trailing_whitespace,
+    },
+  },
+}
+
+local formatGroup =
+  vim.api.nvim_create_augroup("FormatAutogroup", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+  command = ":silent FormatWrite",
+  pattern = "*",
+  group = formatGroup,
+})
+
+-- Telescope
+vim.keymap.set(
+  "n",
+  "<leader>ff",
+  require("telescope.builtin").find_files,
+  { noremap = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader>fg",
+  require("telescope.builtin").live_grep,
+  { noremap = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader>fb",
+  require("telescope.builtin").buffers,
+  { noremap = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader>fh",
+  require("telescope.builtin").help_tags,
+  { noremap = true }
+)
