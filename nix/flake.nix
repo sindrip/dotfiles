ce4bc5abf80dbe7cea34067161b7542aabf80beb
Sindri P. Ingimundarson <sindripetur@gmail.com>
2023-01-24 13:19:55 +0100

    nix profile

---
+++ nix/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Nix home profile";
+
+  # A flake in some other directory.
+  # inputs.otherDir.url = "/home/alice/src/patchelf";
+
+  # The master branch of the NixOS/nixpkgs repository on GitHub.
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+	inherit (nixpkgs.lib) optional;
+	pkgs = import nixpkgs { inherit system; };
+      in {
+	packages.default = pkgs.buildEnv {
+	  name = "Home";
+	  paths = [
+	    #pkgs.glibcLocales
+	    pkgs.ripgrep
+	    pkgs.fd
+	    pkgs.neovim
+
+	    pkgs.beam.packages.erlangR25.elixir_1_14
+
+	    (pkgs.writeScriptBin "upgrade-profile" ''
+	      #!${pkgs.stdenv.shell}
+	      nix profile upgrade '.*'
+	    '')
+	  ];
+	  pathsToLink = [ "/share" "/bin" ];
+	  extraOutputsToInstall = [ "man" "doc" ];
+	};
+      }
+    );
+}
