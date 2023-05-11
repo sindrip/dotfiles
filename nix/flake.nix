7faa72fb2fe4979da02d91f49eff546d787e7a61
Sindri P. Ingimundarson <sindripetur@gmail.com>
2023-05-11 11:44:09 +0200

    Remove sporadic tabs

--- nix/flake.nix
+++ nix/flake.nix
@@ -11,35 +11,34 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-	inherit (nixpkgs.lib) optional;
-	pkgs = import nixpkgs { inherit system; };
+        inherit (nixpkgs.lib) optional;
+        pkgs = import nixpkgs { inherit system; };
       in {
-	packages.default = pkgs.buildEnv {
-	  name = "Home";
-	  paths = [
-	    #pkgs.glibcLocales
-	    pkgs.ripgrep
-	    pkgs.fd
-	    pkgs.neovim
+        packages.default = pkgs.buildEnv {
+          name = "Home";
+          paths = [
+            #pkgs.glibcLocales
+            pkgs.ripgrep
+            pkgs.fd
+            pkgs.neovim
 
-	    pkgs.kubectl
-	    pkgs.kubelogin
-	    pkgs.kubernetes-helm
-	    pkgs.tilt
-	    pkgs.kind
-	    pkgs.ctlptl
-	    pkgs.kustomize
+            pkgs.kubectl
+            pkgs.kubelogin
+            pkgs.kubernetes-helm
+            pkgs.tilt
+            pkgs.kind
+            pkgs.ctlptl
+            pkgs.kustomize
 
-	    pkgs.beam.packages.erlangR25.elixir_1_14
+            pkgs.beam.packages.erlangR25.elixir_1_14
 
-	    (pkgs.writeScriptBin "upgrade-profile" ''
-	      #!${pkgs.stdenv.shell}
-	      nix profile upgrade '.*'
-	    '')
-	  ];
-	  pathsToLink = [ "/share" "/bin" ];
-	  extraOutputsToInstall = [ "man" "doc" ];
-	};
-      }
-    );
+            (pkgs.writeScriptBin "upgrade-profile" ''
+              #!${pkgs.stdenv.shell}
+              nix profile upgrade '.*'
+            '')
+          ];
+          pathsToLink = [ "/share" "/bin" ];
+          extraOutputsToInstall = [ "man" "doc" ];
+        };
+      });
 }
