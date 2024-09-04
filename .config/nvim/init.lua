a85d348b7923c997b910c5eacb820fd7114e259d
Sindri P. Ingimundarson <sindripetur@gmail.com>
2024-09-04 13:16:34 +0200

    nvim: move keymappings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -132,6 +132,13 @@ require("lazy").setup({
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
+    config = function()
+      local builtin = require "telescope.builtin"
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { noremap = true })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { noremap = true })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { noremap = true })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { noremap = true })
+    end,
   },
   {
     "hrsh7th/nvim-cmp",
@@ -260,26 +267,3 @@ require("lspconfig").lua_ls.setup {
     },
   },
 }
-
--- Telescope
-vim.keymap.set("n", "<leader>ff", function()
-  require("telescope.builtin").find_files()
-end, { noremap = true })
-vim.keymap.set(
-  "n",
-  "<leader>fg",
-  require("telescope.builtin").live_grep,
-  { noremap = true }
-)
-vim.keymap.set(
-  "n",
-  "<leader>fb",
-  require("telescope.builtin").buffers,
-  { noremap = true }
-)
-vim.keymap.set(
-  "n",
-  "<leader>fh",
-  require("telescope.builtin").help_tags,
-  { noremap = true }
-)
