8457453677704125cf4d63bbb76ada09f2b178b2
Sindri P. Ingimundarson <sindripetur@gmail.com>
2025-04-11 14:02:39 +0200

    nvim: colorscheme

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -8,6 +8,22 @@ return {
     end,
   },
   {
+    "alexxGmZ/e-ink.nvim",
+    priority = 1000,
+    config = function()
+      require("e-ink").setup()
+      -- vim.cmd.colorscheme "e-ink"
+
+      -- choose light mode or dark mode
+      -- vim.opt.background = "dark"
+      -- vim.opt.background = "light"
+      --
+      -- or do
+      -- :set background=dark
+      -- :set background=light
+    end,
+  },
+  {
     "lewis6991/gitsigns.nvim",
     opts = {},
   },
