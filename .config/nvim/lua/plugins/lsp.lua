439f13f8b84fd9d6fa15245102b777d2697ba2cb
Sindri P. Ingimundarson <sindripetur@gmail.com>
2025-07-03 21:48:07 +0200

    cleanup

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,38 +2,11 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      -- local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-      -- local on_attach = function(_, bufnr)
-      --   -- Enable completion triggered by <c-x><c-o>
-      --   --vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-      --
-      --   -- Mappings.
-      --   -- See `:help vim.lsp.*` for documentation on any of the below functions
-      --   local bufopts = { noremap = true, silent = true, buffer = bufnr }
-      --   --vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
       --   vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
       --   vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-      --   --vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
       --   vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-      --   --vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-      --   --vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-      --   --vim.keymap.set("n", "<space>wl", function()
-      --   --  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      --   --end, bufopts)
-      --   --vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
       --   vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
       --   vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-      --   --vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-      --   --vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
-      --
-      --   vim.lsp.completion.enable(true, client.id, bufnr, {
-      --     autotrigger = true,
-      --     convert = function(item)
-      --       return { abbr = item.label:gsub("%b()", "") }
-      --     end,
-      --   })
-      -- end
 
       vim.lsp.config("elixirls", {
         -- Unix
@@ -94,31 +67,6 @@ return {
       })
 
       vim.lsp.enable { "elixirls", "rust_analyzer", "nixd", "lua_ls" }
-
-      -- require("lspconfig").lua_ls.setup {
-      --   on_attach = on_attach,
-      --   capabilities = capabilities,
-      --   settings = {
-      --     Lua = {
-      --       runtime = {
-      --         version = "LuaJIT",
-      --       },
-      --       diagnostics = {
-      --         disable = { "missings-fields" },
-      --       },
-      --       workspace = {
-      --         checkThirdParty = false,
-      --         library = {
-      --           vim.env.VIMRUNTIME,
-      --           "${3rd}/luv/library",
-      --         },
-      --       },
-      --       telemetry = {
-      --         enable = false,
-      --       },
-      --     },
-      --   },
-      -- }
     end,
   },
 }
