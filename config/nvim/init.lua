6d7faad34afdb214a617d6aa4f9df22fb2ab8f64
Sindri P. Ingimundarson <sindripetur@gmail.com>
2026-01-17 02:30:08 +0100

    nvim: conform.nvim etc.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,48 +1,172 @@
--- Leader key
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+require("config.lazy")
 
+-- vim.opt.softtabstop = -1
 -- Options
 vim.o.swapfile = false
 vim.o.autoread = true
--- TODO: Set number and relativenumber
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.scrolloff = 3
+vim.o.sidescrolloff = 5
+vim.o.cursorline = true
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.shiftwidth = 2
+vim.o.shiftround = true
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.smartindent = true
+vim.o.undofile = true
+-- vim.o.undolevels = 10000
+-- vim.opt.signcolumn = "number"
+vim.o.signcolumn = "yes"
+vim.o.laststatus = 3
+vim.o.list = true
+vim.opt.listchars = {
+  tab = "→ ",
+  nbsp = "␣",
+  trail = "·",
+  -- space = "·",
+  extends = "⟩",
+  precedes = "⟨",
+}
+-- vim.opt.completeopt = { "menu", "menuone", "noinsert", "noselect" }
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  pattern = "*",
+  desc = "highlight selection on yank",
+  callback = function()
+    vim.hl.on_yank({ timeout = 200 })
+  end,
+})
+
+-- open help in vertical split
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = vim.api.nvim_create_augroup("help_vertical", { clear = true }),
+  callback = function()
+    if vim.bo.buftype == "help" then
+      vim.cmd.wincmd("L")
+    end
+  end,
+})
+
+-- LSP
+vim.lsp.enable("bashls")
+vim.lsp.enable("lua_ls")
+
+vim.diagnostic.config({
+  virtual_text = true,
+  -- virtual_lines = {
+  --         current_line = false
+  -- }
+})
 
 -- Setup plugins
 require("lazy").setup({
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "frappe", -- latte, frappe, macchiato, mocha
-        transparent_background = false,
-      })
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find grep" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "find help" })
-    end,
+  spec = {
+    { import = "plugins" },
+    {
+      "catppuccin/nvim",
+      name = "catppuccin",
+      priority = 1000,
+      config = function()
+        require("catppuccin").setup({
+          flavour = "frappe", -- latte, frappe, macchiato, mocha
+          transparent_background = false,
+        })
+        vim.cmd.colorscheme("catppuccin")
+      end,
+    },
+
+    {
+      "nvim-telescope/telescope.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find grep" })
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help" })
+      end,
+    },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      lazy = false,
+      build = ":TSUpdate",
+    },
+    { "neovim/nvim-lspconfig" },
+    { "folke/which-key.nvim" },
+    { "github/copilot.vim" },
+
+    {
+      "saghen/blink.cmp",
+      dependencies = {},
+
+      -- use a release tag to download pre-built binaries
+      version = "1.*",
+
+      ---@module 'blink.cmp'
+      ---@type blink.cmp.Config
+      opts = {
+        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+        -- 'super-tab' for mappings similar to vscode (tab to accept)
+        -- 'enter' for enter to accept
+        -- 'none' for no mappings
+        -- All presets have the following mappings:
+        -- C-space: Open menu or open docs if already open
+        -- C-n/C-p or Up/Down: Select next/previous item
+        -- C-e: Hide menu
+        -- C-k: Toggle signature help (if signature.enabled = true)
+        --
+        -- See :h blink-cmp-config-keymap for defining your own keymap
+        keymap = { preset = "default" },
+        appearance = {
+          nerd_font_variant = "mono",
+        },
+        completion = { documentation = { auto_show = true } },
+        sources = {
+          default = { "lsp", "path", "snippets", "buffer" },
+        },
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+      },
+      opts_extend = { "sources.default" },
+    },
+
+    ---------------------------------------------
+    -- UI
+    {
+      "folke/snacks.nvim",
+      priority = 1000,
+      lazy = false,
+      ---@type snacks.Config
+      opts = {
+        -- bigfile = { enabled = true },
+        -- dashboard = { enabled = true },
+        -- explorer = { enabled = true },
+        indent = { enabled = true },
+        -- input = { enabled = true },
+        picker = { enabled = true },
+        notifier = { enabled = true },
+        -- quickfile = { enabled = true },
+        -- scope = { enabled = true },
+        -- scroll = { enabled = true },
+        -- statuscolumn = { enabled = true },
+        -- words = { enabled = true },
+      },
+    },
+
+    -- {
+    -- enabled = false,
+    --   "folke/noice.nvim",
+    -- },
+    --
   },
 })
+
+-- Keymaps
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
---
+++ config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,77 @@
+local CreateToggleModule = function(name)
+  local M = {}
+
+  M[name] = true
+
+  M.toggle = function()
+    M[name] = not M[name]
+    if M[name] then
+      vim.notify("Enabled " .. name .. " (Global)")
+    else
+      vim.notify("Disabled " .. name .. " (Global)")
+    end
+  end
+
+  M.is_enabled = function()
+    return M[name]
+  end
+
+  M.icon = function()
+    local icon = M[name] and " " or " "
+    local color = M[name] and "green" or "yellow"
+    return {
+      icon = icon,
+      color = color,
+    }
+  end
+
+  M.desc = function()
+    return M[name] and "Disable " .. name or "Enable " .. name
+  end
+
+  return M
+end
+
+local autoformat = CreateToggleModule("Auto Format")
+
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      format_on_save = function()
+        if autoformat.is_enabled() then
+          return {
+            timeout_ms = 1000,
+            lsp_format = "fallback",
+          }
+        else
+          return nil
+        end
+      end,
+      formatters_by_ft = {
+        -- lua = { "stylua" },
+        -- python = { "black" },
+        -- javascript = { "prettierd" },
+        -- typescript = { "prettierd" },
+        -- html = { "prettierd" },
+        -- css = { "prettierd" },
+        -- json = { "prettierd" },
+        -- markdown = { "prettierd" },
+      },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>u", group = "+ui" },
+        {
+          "<leader>uf",
+          autoformat.toggle,
+          icon = autoformat.icon,
+          desc = autoformat.desc,
+        },
+      },
+    },
+  },
+}
