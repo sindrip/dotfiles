d623bc43d827c8b83e0e8538cea2a4a444c02160
Sindri P. Ingimundarson <sindripetur@gmail.com>
2026-01-21 14:48:53 +0100

    cleanup

--- config/vscode-nvim/init.lua
+++ config/vscode-nvim/init.lua
@@ -1,56 +1,24 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
-
 vim.o.undofile = true
 vim.o.autoread = true -- Is this needed?
 
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
-local vscode = require('vscode')
-
 -- Vim notifications are displayed in the VSCode notification system
+local vscode = require('vscode')
 vim.notify = vscode.notify
 
--- Keymaps
 vim.keymap.set('n', '<ESC>', ':nohlsearch<CR>', { desc = 'Clear search highlighting' })
-
--- vim.keymap.set({ "n", "t" }, "<leader>t", function()
---     vscode.action('workbench.action.createTerminalEditor')
--- end, { desc = "Open terminal" })
-
-
+vim.keymap.set('n', '<leader>un', function() vscode.action('notifications.clearAll') end, { desc = 'Dismiss Notifications' })
+vim.keymap.set('n', '<leader>ff', function() vscode.action('workbench.action.quickOpen') end, { desc = 'Find files' })
+vim.keymap.set('n', '<leader>fg', function() vscode.action('workbench.action.findInFiles') end, { desc = 'Find grep' })
 vim.keymap.set('n', '<leader>r', function()
     vim.notify('Restarting Neovim extension...')
     vscode.action('vscode-neovim.restart')
 end, { desc = 'Restart neovim extension' })
-vim.keymap.set('n', '<leader>un', function()
-        vscode.action('notifications.clearAll')
-    end,
-    { desc = 'Dismiss Notifications' })
-vim.keymap.set('n', '<leader>ut', function()
-        if vscode.get_config('workbench.editor.showTabs') == 'none' then
-            vscode.update_config('workbench.editor.showTabs', 'multiple', 'global')
-        else
-            vscode.update_config('workbench.editor.showTabs', 'none', 'global')
-        end
-    end,
-    { desc = 'Toggle tabs' })
-
--- "Telescope"
-vim.keymap.set('n', '<leader>ff', function()
-        vscode.action('workbench.action.quickOpen')
-    end,
-    { desc = 'Find files' })
--- vim.keymap.set('n', '<leader>fg', function()
---         vscode.action('workbench.action.findInFiles')
---     end,
---     { desc = 'Find grep' })
-vim.keymap.set('n', '<leader>fg', function()
-        vscode.action('television.ToggleTextFinder')
-    end,
-    { desc = 'Find grep' })
 
 -- AutoCommands
 vim.api.nvim_create_autocmd('TextYankPost', {
