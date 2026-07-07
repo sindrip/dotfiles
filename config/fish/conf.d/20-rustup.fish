b5cbfcffb312baa36bce563be75ab01629eba5b8
Sindri P. Ingimundarson <sindripetur@gmail.com>
2026-07-07 23:54:07 +0200

    refactor(fish): import zshenv instead of mirroring it

--- config/fish/conf.d/20-rustup.fish
+++
@@ -1,5 +0,0 @@
-if test -f "$CARGO_HOME/env.fish"
-    source "$CARGO_HOME/env.fish"
-else if test -d "$CARGO_HOME/bin"; and not contains -- "$CARGO_HOME/bin" $PATH
-    set -gx PATH "$CARGO_HOME/bin" $PATH
-end
